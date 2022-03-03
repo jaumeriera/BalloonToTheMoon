313812aa20aab0fca98c3f2838610c8f5b987648
Jaume Riera <jaumerg95@gmail.com>
2022-03-03 11:35:57 +0100

    Remove check enemy from destroy zone

--- Assets/Scripts/Common/DestroyZone.cs
+++ Assets/Scripts/Common/DestroyZone.cs
@@ -6,16 +6,6 @@ public class DestroyZone : MonoBehaviour
 {
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (IsSideEnemy(collision))
-        {
-            EnemySpawnerManager.spawnedInSide -= 1;
-        }
         collision.gameObject.SetActive(false);
-    }
-
-    private bool IsSideEnemy(Collider2D collision)
-    {
-        BaseEnemy enemy = collision.GetComponent<BaseEnemy>();
-        return enemy != null && enemy.type == BaseEnemy.enemyType.side;
     }
 }
